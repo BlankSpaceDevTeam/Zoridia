c00eb4ab12975c80ee588e2d8b63a9dff7c1c6db
Nathan S <damacamoo@gmail.com>
2016-04-03 15:16:56 -0700

    Added walls, and double jump functionality -Nathan S

--- Zoridia/Assets/Scripts/FightManager.cs
+++ Zoridia/Assets/Scripts/FightManager.cs
@@ -10,7 +10,13 @@ public class FightManager : MonoBehaviour {
     GameObject player1;
     GameObject player2;
 
-    
+
+    //keydelay keeps ketboard form inputting controls too quickly(once per frame aka as fast as possible)
+    float keyDelay1 = 0.2f;  // 0.2 seconds
+    float timePassed1 = 0.0f;
+    float keyDelay2 = 0.2f;  // 0.2 seconds
+    float timePassed2 = 0.0f;
+
     //player move speeds
     float player1moveSpeed = 0.5f;
     float player2moveSpeed = 0.5f;
@@ -26,6 +32,10 @@ public class FightManager : MonoBehaviour {
     //player acceleration
     Vector3 player1Accel;
     Vector3 player2Accel;
+
+    //player jump number
+    int player1Jumps = 2;
+    int player2Jumps = 2;
 
     //controller deadzones(1 for first player, 2 for second player)
     float deadZone1 = 0.6f;
@@ -56,16 +66,19 @@ public class FightManager : MonoBehaviour {
 
     // Update is called once per frame
     void Update () {
-        //note: alot of values use 5.0f, this is half the height of the player, and will need to be replaced with player1.height/2 eventually
+        //note: alot of values use 5.0f, this is half the height of the (cube)player, and will need to be replaced with player1.height/2 eventually
         //INPUT CONTROLLER
         //Player1 = XBOX CONTROLLER
         //Player 2 = KEYBOARD
         //LeftstickX1 means left analog stick, x-axis, controller1
 
+        //keydelays, update every frame
+        timePassed1 += Time.deltaTime;
+        timePassed2 += Time.deltaTime;
 
         //PLAYER 1
         //ensure player cant fall off map
-        if (player1.transform.position.y != 5.0f) //player is at bottom of map
+        if (player1.transform.position.y != 5.0f) //player isnt on ground
         {
             if (player1.transform.position.y < 5.0f) //player is below bottom of map
             {
@@ -77,6 +90,9 @@ public class FightManager : MonoBehaviour {
             {
                 player1Accel.y = -9.81f * 15; 
             }
+        }else //player is on ground, so reset jumps to 2
+        {
+            player1Jumps = 2;
         }
         
 
@@ -91,17 +107,19 @@ public class FightManager : MonoBehaviour {
         }
         
         //input manager
-        if (Input.GetAxis("LeftStickX1") > deadZone1)
+        if (Input.GetAxis("LeftStickX1") > deadZone1 && player1.transform.position.x < 32)
         {
             player1.transform.position = new Vector3(player1.transform.position.x + player1moveSpeed, player1.transform.position.y, player1.transform.position.z);
         }
-        if (Input.GetAxis("LeftStickX1") < (deadZone1 * -1.0f))
+        if (Input.GetAxis("LeftStickX1") < (deadZone1 * -1.0f) && player1.transform.position.x > -32 )
         {
             player1.transform.position = new Vector3(player1.transform.position.x - player1moveSpeed, player1.transform.position.y, player1.transform.position.z);
         }
-        if (Input.GetKey(KeyCode.JoystickButton1) /*&& TODO:player is on platform*/)
+        if (Input.GetKey(KeyCode.JoystickButton1) && player1Jumps > 1 && timePassed1 >= keyDelay1)
         {
+            player1Jumps--;
             player1Veloc = new Vector3(0.0f, player1jumpSpeed, 0.0f);
+            timePassed1 = 0.0f;
         }
 
 
@@ -121,6 +139,10 @@ public class FightManager : MonoBehaviour {
                 player2Accel.y = -9.81f * 15;
             }
         }
+        else //player is on ground, so reset jumps to 2
+        {
+            player2Jumps = 2;
+        }
 
 
 
@@ -133,19 +155,28 @@ public class FightManager : MonoBehaviour {
             player2.transform.position = new Vector3(player2.transform.position.x + player2Veloc.x * Time.deltaTime, player2.transform.position.y + player2Veloc.y * Time.deltaTime, player2.transform.position.z + player2Veloc.z * Time.deltaTime);
         }
 
+
+
+         
+        
         //input manager
-        if (Input.GetKey(KeyCode.A))
+
+        if (Input.GetKey(KeyCode.A) && player2.transform.position.x > -32)
         {
             player2.transform.position = new Vector3(player2.transform.position.x - player2moveSpeed, player2.transform.position.y, player2.transform.position.z);
         }
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.D) && player2.transform.position.x < 32)
         {
             player2.transform.position = new Vector3(player2.transform.position.x + player2moveSpeed, player2.transform.position.y, player2.transform.position.z);
         }
-        if (Input.GetKey(KeyCode.Space) /*&& TODO:player is on platform*/)
+        if (Input.GetKey(KeyCode.Space) && player2Jumps > 1 && timePassed2 >= keyDelay2)
         {
+            player2Jumps--;
             player2Veloc = new Vector3(0.0f, player2jumpSpeed, 0.0f);
+            timePassed2 = 0.0f;
         }
+        
+        
         
 
     }
